8672f5b7d17232e9b24ba9dba0c04b3d239f6433
Amber <jayhrod26@gmail.com>
2023-03-12 19:59:12 -0400

    setting up functions for choosing colors

--- colorfinder.js
+++ colorfinder.js
@@ -1,3 +1,17 @@
 const rightButton = document.getElementById('rightButton')
 const leftButton = document.getElementById('leftButton')
 
+var color = "#000000";
+
+function randomColor() {
+    color = "#";
+    color += "aa1122";
+    return color;
+}
+
+function setLeftColor() {
+    leftButton.style.backgroundColor = randomColor();
+}
+function setRightColor() {
+    rightButton.style.backgroundColor = randomColor();
+}
