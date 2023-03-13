b61f08415b2e241f62073bc02e8889a69846203b
Amber <jayhrod26@gmail.com>
2023-03-12 20:17:17 -0400

    added support for hex colors

--- colorfinder.js
+++ colorfinder.js
@@ -2,10 +2,37 @@ const rightButton = document.getElementById('rightButton')
 const leftButton = document.getElementById('leftButton')
 
 var color = "#000000";
+var leftColor = "";
+var rightColor = "";
 
 function randomColor() {
     color = "#";
-    color += "aa1122";
+    var tempColor = "";
+    for (let i = 0; i < 6; i++) {
+        tempColor = (Math.floor(Math.random() * 16));
+        switch(tempColor) {
+            case 15:
+                tempColor = "F";
+                break;
+            case 14:
+                tempColor = "E";
+                break;
+            case 13:
+                tempColor = "D";
+                break;
+            case 12:
+                tempColor = "C";
+                break;
+            case 11:
+                tempColor = "B";
+                break;
+            case 10:
+                tempColor = "A";
+                break;
+        }
+        color += tempColor;
+    }
+    console.log(color);
     return color;
 }
 
@@ -15,3 +42,5 @@ function setLeftColor() {
 function setRightColor() {
     rightButton.style.backgroundColor = randomColor();
 }
+
+setLeftColor(); setRightColor();
